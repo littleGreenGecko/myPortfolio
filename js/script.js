423302bf3a3aa108657934117c97b0c1a44c0f7c
Ewa <ewa.baker@yahoo.com>
2021-11-09 16:39:37 +0000

    Change file name and Create function for responsive menu bar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function navIcon() {
+    let links = document.getElementById("menu");
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+  }
